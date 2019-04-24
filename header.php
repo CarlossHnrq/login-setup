852fa2186d5f8572bfffb858b006345b4465e6f9
Carlos Henrique <carlos_hnrq@hotmail.com>
2019-04-24 14:46:12 -0300

    Add HTML and CSS To project

---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="this is an example of a meta description. This will often show up in search results.">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<header class="menu-bg">
+			<nav class="menu">
+				<div class="menu-nav">	
+					<a href="#" class="logo"><h1>Login-Setup</h1></a>
+					<ul class="lista">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="#">Portfolio</a></li>
+						<li><a href="#">About me</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</div>
+				<div class="login-form">
+					<form action="includes/login.inc.php" method="post">
+						<input type="text" name="mailuid" placeholder="Username/E-mail...">
+						<input type="password" name="pwd" placeholder="Password...">
+						<button type="submit" name="login-submit">Login</button>
+					</form>
+					<a href="signup.php">Signup</a>
+					<form action="includes/logout.inc.php" method="post">
+						<button type="submit" name="logout-submit">Logout</button>
+					</form>
+				</div>
+			</nav>
+		</header>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+	require "header.php";
+?>
+
+	<main>
+		<div class="main-content">
+			<section>
+				<p>You are logged out!</p>
+				<p>You are logged in!</p>
+			</section>
+		</div>
+	</main>
+
+<?php 
+	require "footer.php";
+?>
